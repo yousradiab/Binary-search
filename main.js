f8ae8a2f9a6f8d387ffaaf5d7c5d0cae3fc1c6c1
Yousra Diab <yousserah@gmail.com>
2024-09-02 19:21:33 +0200

    øvelse færdig

--- main.js
+++ main.js
@@ -1,20 +1,37 @@
 "use strict";
-const values = [21, 22, 23, 25, 27, 28, 29, 31, 32, 34, 35];
+const numbers = [21, 22, 23, 25, 27, 28, 29, 31, 32, 34, 35];
+const ordliste = [
+  "aarhus",
+  "andedam",
+  "andegård",
+  "bondefanget",
+  "bondegård",
+  "børnearbejde",
+  "gadefejer",
+  "gær",
+  "gødning",
+  "gaardejer",
+  "kalapøjser",
+  "kalundborg",
+  "kørt",
+  "kårde",
+  "ålborg",
+];
 
-function binarySearch(search, values) {
+function binarySearch(search, values, comfunc) {
   let min = 0;
   let max = values.length - 1;
   let found = false;
 
   while (!found && min <= max) {
     let middle = Math.floor((min + max) / 2);
-    let c = compare(search, values[middle]);
+    let c = comfunc(search, values[middle]);
 
     console.log(`min: ${min}, mid: ${middle}, max: ${max}, c: ${c}`);
 
     if (c === 0) {
       found = true;
-      return middle;
+      return values[middle];
     }
     if (c > 0) {
       min = middle + 1;
@@ -25,9 +42,13 @@ function binarySearch(search, values) {
   return -1;
 }
 
+function stringcomparefunction(a, b) {
+  return a.localeCompare(b);
+}
+console.log(binarySearch("gaardejer", ordliste, stringcomparefunction));
+
 function compare(a, b) {
   return a - b;
 }
 
-let index = binarySearch(28, values);
-console.log(`Fundet på index: ${index}`);
+console.log(binarySearch(28, numbers, compare));
